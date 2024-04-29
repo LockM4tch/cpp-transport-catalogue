99228e0d5153e1b8d983a106fac831babb23aa2b
LockM4tch <123293682+LockM4tch@users.noreply.github.com>
2024-04-29 19:19:08 +0300

    Update input_reader.cpp
    spacing

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,95 +1,93 @@
 #include "input_reader.h"
 
-
-
 namespace InputReaderParser {
-    //Парсит строку вида "10.123,  -30.1837" и возвращает пару координат (широта, долгота)
+    //ГЏГ Г°Г±ГЁГІ Г±ГІГ°Г®ГЄГі ГўГЁГ¤Г  "10.123,  -30.1837" ГЁ ГўГ®Г§ГўГ°Г Г№Г ГҐГІ ГЇГ Г°Гі ГЄГ®Г®Г°Г¤ГЁГ­Г ГІ (ГёГЁГ°Г®ГІГ , Г¤Г®Г«ГЈГ®ГІГ )
     geo::Coordinates ParseCoordinates(std::string_view str) {
-            static const double nan = std::nan("");
+        static const double nan = std::nan("");
 
-            auto not_space = str.find_first_not_of(' ');
-            auto comma = str.find(',');
+        auto not_space = str.find_first_not_of(' ');
+        auto comma = str.find(',');
 
-            if (comma == str.npos) {
-                return { nan, nan };
-            }
-
-            auto not_space2 = str.find_first_not_of(' ', comma + 1);
-
-            double lat = std::stod(std::string(str.substr(not_space, comma - not_space)));
-            double lng = std::stod(std::string(str.substr(not_space2)));
-
-            return { lat, lng };
+        if (comma == str.npos) {
+            return { nan, nan };
         }
 
-    //Удаляет пробелы в начале и конце строки
+        auto not_space2 = str.find_first_not_of(' ', comma + 1);
+
+        double lat = std::stod(std::string(str.substr(not_space, comma - not_space)));
+        double lng = std::stod(std::string(str.substr(not_space2)));
+
+        return { lat, lng };
+    }
+
+    //Г“Г¤Г Г«ГїГҐГІ ГЇГ°Г®ГЎГҐГ«Г» Гў Г­Г Г·Г Г«ГҐ ГЁ ГЄГ®Г­Г¶ГҐ Г±ГІГ°Г®ГЄГЁ
     std::string_view Trim(std::string_view string) {
-            const auto start = string.find_first_not_of(' ');
-            if (start == string.npos) {
-                return {};
-            }
-            return string.substr(start, string.find_last_not_of(' ') + 1 - start);
+        const auto start = string.find_first_not_of(' ');
+        if (start == string.npos) {
+            return {};
         }
+        return string.substr(start, string.find_last_not_of(' ') + 1 - start);
+    }
 
-    //Разбивает строку string на n строк, с помощью указанного символа-разделителя delim
+    //ГђГ Г§ГЎГЁГўГ ГҐГІ Г±ГІГ°Г®ГЄГі string Г­Г  n Г±ГІГ°Г®ГЄ, Г± ГЇГ®Г¬Г®Г№ГјГѕ ГіГЄГ Г§Г Г­Г­Г®ГЈГ® Г±ГЁГ¬ГўГ®Г«Г -Г°Г Г§Г¤ГҐГ«ГЁГІГҐГ«Гї delim
     std::vector<std::string_view> Split(std::string_view string, char delim) {
-            std::vector<std::string_view> result;
+        std::vector<std::string_view> result;
 
-            size_t pos = 0;
-            while ((pos = string.find_first_not_of(' ', pos)) < string.length()) {
-                auto delim_pos = string.find(delim, pos);
-                if (delim_pos == string.npos) {
-                    delim_pos = string.size();
-                }
-                if (auto substr = Trim(string.substr(pos, delim_pos - pos)); !substr.empty()) {
-                    result.push_back(substr);
-                }
-                pos = delim_pos + 1;
+        size_t pos = 0;
+        while ((pos = string.find_first_not_of(' ', pos)) < string.length()) {
+            auto delim_pos = string.find(delim, pos);
+            if (delim_pos == string.npos) {
+                delim_pos = string.size();
             }
-
-            return result;
+            if (auto substr = Trim(string.substr(pos, delim_pos - pos)); !substr.empty()) {
+                result.push_back(substr);
+            }
+            pos = delim_pos + 1;
         }
+
+        return result;
+    }
 
     /**
-    * Парсит маршрут.
-    * Для кольцевого маршрута (A>B>C>A) возвращает массив названий остановок [A,B,C,A]
-    * Для некольцевого маршрута (A-B-C-D) возвращает массив названий остановок [A,B,C,D,C,B,A]
+    * ГЏГ Г°Г±ГЁГІ Г¬Г Г°ГёГ°ГіГІ.
+    * Г„Г«Гї ГЄГ®Г«ГјГ¶ГҐГўГ®ГЈГ® Г¬Г Г°ГёГ°ГіГІГ  (A>B>C>A) ГўГ®Г§ГўГ°Г Г№Г ГҐГІ Г¬Г Г±Г±ГЁГў Г­Г Г§ГўГ Г­ГЁГ© Г®Г±ГІГ Г­Г®ГўГ®ГЄ [A,B,C,A]
+    * Г„Г«Гї Г­ГҐГЄГ®Г«ГјГ¶ГҐГўГ®ГЈГ® Г¬Г Г°ГёГ°ГіГІГ  (A-B-C-D) ГўГ®Г§ГўГ°Г Г№Г ГҐГІ Г¬Г Г±Г±ГЁГў Г­Г Г§ГўГ Г­ГЁГ© Г®Г±ГІГ Г­Г®ГўГ®ГЄ [A,B,C,D,C,B,A]
     */
     std::vector<std::string_view> ParseRoute(std::string_view route) {
-            if (route.find('>') != route.npos) {
-                return Split(route, '>');
-            }
-
-            auto stops = Split(route, '-');
-            std::vector<std::string_view> results(stops.begin(), stops.end());
-            results.insert(results.end(), std::next(stops.rbegin()), stops.rend());
-
-            return results;
+        if (route.find('>') != route.npos) {
+            return Split(route, '>');
         }
+
+        auto stops = Split(route, '-');
+        std::vector<std::string_view> results(stops.begin(), stops.end());
+        results.insert(results.end(), std::next(stops.rbegin()), stops.rend());
+
+        return results;
+    }
 
     CommandDescription ParseCommandDescription(std::string_view line) {
-            auto colon_pos = line.find(':');
-            if (colon_pos == line.npos) {
-                return {};
-            }
-
-            auto space_pos = line.find(' ');
-            if (space_pos >= colon_pos) {
-                return {};
-            }
-
-            auto not_space = line.find_first_not_of(' ', space_pos);
-            if (not_space >= colon_pos) {
-                return {};
-            }
-
-            return { std::string(line.substr(0, space_pos)),
-                    std::string(Trim(line.substr(not_space, colon_pos - not_space))),
-                    std::string(line.substr(colon_pos + 1)) };
+        auto colon_pos = line.find(':');
+        if (colon_pos == line.npos) {
+            return {};
         }
+
+        auto space_pos = line.find(' ');
+        if (space_pos >= colon_pos) {
+            return {};
+        }
+
+        auto not_space = line.find_first_not_of(' ', space_pos);
+        if (not_space >= colon_pos) {
+            return {};
+        }
+
+        return {std::string(line.substr(0, space_pos)),
+                std::string(Trim(line.substr(not_space, colon_pos - not_space))),
+                std::string(line.substr(colon_pos + 1)) };
+    }
 }
 
-void FillCatalogue(std::istream& in, TransportCatalogue& catalogue){
+void FillCatalogue(std::istream& in, TransportCatalogue& catalogue) {
     int base_request_count;
     in >> base_request_count >> std::ws;
 
@@ -103,16 +101,16 @@ void FillCatalogue(std::istream& in, TransportCatalogue& catalogue){
 }
 
 void InputReader::ParseLine(std::string_view line) {
-        auto command_description = InputReaderParser::ParseCommandDescription(InputReaderParser::Trim(line));
-        if (command_description) {
-            commands_.push_back(std::move(command_description));
-        }
+    auto command_description = InputReaderParser::ParseCommandDescription(InputReaderParser::Trim(line));
+    if (command_description) {
+        commands_.push_back(std::move(command_description));
+    }
 }
 
 void InputReader::ApplyCommands([[maybe_unused]] TransportCatalogue& catalogue) const {
     for (auto command : commands_) {
-        if (command.command == "Stop") { 
-            catalogue.AddStop(command.id, InputReaderParser::ParseCoordinates(command.description) );
+        if (command.command == "Stop") {
+            catalogue.AddStop(command.id, InputReaderParser::ParseCoordinates(command.description));
         }
         else if (command.command == "Bus") {
             catalogue.AddBus(command.id, InputReaderParser::ParseRoute(command.description));
